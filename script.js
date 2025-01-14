3858734c6dbd9cb4b2b369ac6154958e64584859
Hugo <hugowinged@gmail.com>
2025-01-13 22:02:30 -0800

    Added logic to dynamically assign size to drawing area and square pixels

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 'use strict'
 
-const container = document.querySelector('.container');
+// SIZE DIMENSIONS OF DRAW AREA -WIDTH AND HEIGHT-
+const drawAreaWidthHeight = 800;
+const drawArea = document.querySelector('.drawArea');
+
+// SETS SIZE DIMENSIONS OF DRAW AREA DYNAMICALLY
+drawArea.style.width = `${drawAreaWidthHeight}px`;
+drawArea.style.height = `${drawAreaWidthHeight}px`;
+
+function createPixel(pixelQty) {
+    for(let p = 0; p < pixelQty * pixelQty; p++) {
+        const createPixel = document.createElement('div');
+
+        createPixel.style.width = `${(drawAreaWidthHeight / pixelQty)}px`;
+        createPixel.style.height = `${(drawAreaWidthHeight / pixelQty)}px`;
+        
+        createPixel.classList.add('pixelStyle');
+        drawArea.appendChild(createPixel);
+    }
+}
+
+createPixel(16);
